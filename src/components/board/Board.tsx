fce7c94eae98967217d7a49dd0dfa36bd5835cbc
akhiljozph <ajpulluvelil@gmail.com>
2025-11-13 23:28:27 +0530

    TTT-10 - Tic-tac-toe implementation.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -34,6 +34,33 @@ function Board() {
 
     return (
         <section className="tic-tac-wrapper">
+            <header>Tic Tac Toe</header>
+            <section className="tic-tac-players-information">
+                <div className="players-name">
+                    <div>
+                        <label htmlFor="player-one">Player 1: </label>
+                        <input id="player-one" className="tic-tac-input" maxLength={10}/>
+                    </div>
+                    <div>
+                        <label htmlFor="player-two">Player 2: </label>
+                        <input id="player-two" className="tic-tac-input" maxLength={10}/>
+                    </div>
+                </div>
+                <div>
+                    <table>
+                        <tr>
+                            <th>Player 1</th>
+                            <th>Player 2</th>
+                            <th>Draw</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                        </tr>
+                    </table>
+                </div>
+            </section>
             <span className="tic-tac-alert">Player 1 won!</span>
             <div className="tic-tac-board">
                 {cells.map((cell, cellIndex) => (
