1665cfa3ca8922c05af68600c7072dc9c56b45d2
akhiljozph <ajpulluvelil@gmail.com>
2025-11-15 15:32:58 +0530

    TTT-11 - Tic-tac-toe implementation.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -34,41 +34,69 @@ function Board() {
 
     return (
         <section className="tic-tac-wrapper">
-            <header>Tic Tac Toe</header>
+            <header>
+                <h1>Tic Tac Toe</h1>
+            </header>
+
             <section className="tic-tac-players-information">
-                <div className="players-name">
+                <form className="players-name">
                     <div>
-                        <label htmlFor="player-one">Player 1: </label>
-                        <input id="player-one" className="tic-tac-input" maxLength={10}/>
+                        <label htmlFor="player-one">Player 1:&nbsp;</label>
+                        <input
+                            id="player-one"
+                            className="tic-tac-input"
+                            maxLength={10}
+                        />
                     </div>
+
                     <div>
-                        <label htmlFor="player-two">Player 2: </label>
-                        <input id="player-two" className="tic-tac-input" maxLength={10}/>
+                        <label htmlFor="player-two">Player 2:&nbsp;</label>
+                        <input
+                            id="player-two"
+                            className="tic-tac-input"
+                            maxLength={10}
+                        />
                     </div>
-                </div>
-                <div>
+                </form>
+
+                <section aria-labelledby="scoreboard-title">
+                    <h2 id="scoreboard-title" className="visually-hidden">Scoreboard</h2>
                     <table>
-                        <tr>
-                            <th>Player 1</th>
-                            <th>Player 2</th>
-                            <th>Draw</th>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>1</td>
-                            <td>1</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th scope="col">Player 1</th>
+                                <th scope="col">Player 2</th>
+                                <th scope="col">Draw</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
                     </table>
-                </div>
+                </section>
             </section>
-            <span className="tic-tac-alert">Player 1 won!</span>
-            <div className="tic-tac-board">
+
+            <p role="alert" className="tic-tac-alert">Player 1 won!</p>
+
+            <section className="tic-tac-board" aria-label="Game board">
                 {cells.map((cell, cellIndex) => (
-                    <Cell key={cellIndex} onCellClick={() => handleClick(cellIndex)} value={cell} />
+                    <Cell
+                        key={cellIndex}
+                        onCellClick={() => handleClick(cellIndex)}
+                        value={cell}
+                    />
                 ))}
-            </div>
-            <button className="reset-btn" onClick={resetProgress}>Reset Game</button>
+            </section>
+
+            <button className="reset-btn" onClick={resetProgress}>
+                Reset Game
+            </button>
         </section>
+
     )
 }
 export default Board;
